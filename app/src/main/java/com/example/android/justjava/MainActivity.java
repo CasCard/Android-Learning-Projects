e5224facc5b3c7f8be0f16eb69acf7ce4c5ab73d
Abel C Dixon <abelcheruvathoor@gmail.com>
2019-11-27 13:10:23 +0530

    Updated Final Coffee ordering app

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,17 +9,35 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int numberOfCoffees=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view) {
-        displayPrice(2*5);
-        display(2);
+        String priceMessagae="Total "+numberOfCoffees*5+"\nThank You";
+        displayMessage(priceMessagae);
+       displayPrice(numberOfCoffees*5);
+        display(numberOfCoffees);
+    }
+    public void increment(View view) {
+
+        numberOfCoffees=numberOfCoffees+1;
+//        displayPrice(numberOfCoffees*5);
+        display(numberOfCoffees);
+    }
+    public void decrement(View view) {
+
+        numberOfCoffees=numberOfCoffees-1;
+//        displayPrice(numberOfCoffees*5);
+        display(numberOfCoffees);
     }
 
+    private void displayMessage(String message) {
+        TextView quantityTextView = (TextView) findViewById(R.id.comments);
+        quantityTextView.setText(message);
+    }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
